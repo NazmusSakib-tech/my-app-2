742d50c55752812e904ffa76a2862931f0cd1f5a
Nazmus Sakib <sakibkhancse09@gmail.com>
2021-09-22 19:25:21 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Cosmetics from './Cosmetics/Cosmetics';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1>Assalamualaikum</h1>
+      <Cosmetics></Cosmetics>
+    </div>
+  );
+}
+
+export default App;
